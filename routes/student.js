55c492b6289a533e0fa2e59d364a2acce3043079
Aishik Deb <debaishik14@gmail.com>
2021-05-21 15:12:24 +0530

    student portal backend

---
+++ routes/student.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+var students = require('../database');
+
+router.route('/').get((req,res) => {
+    res.json(students);
+});
+
+router.route('/:id').get((req,res) => {
+    const id = req.params.id;
+    const student = students.find((student) => student.id == id);
+    res.json(student);
+});
+
+router.route('/update').post((req,res) => {
+    const student = req.body;
+    const index = students.findIndex((element) => element.id == student.id);
+    students[index] = student;
+    res.json("updated student");
+});
+
+router.route('/delete/:id').post((req,res) => {
+    const id = req.params.id;
+    students = students.filter(student => student.id != id);
+    res.json("deleted student");
+});
+
+router.route('/add').post((req,res) => {
+    const student = req.body;
+    const newId = students.length ? students[students.length-1].id + 1 : 1;
+    var newStudent = student;
+    newStudent["id"] = newId;
+    students.push(newStudent);
+    res.json(newStudent);
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const basicAuth = require('express-basic-auth');
+const http = require('http');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const startTime = new Date();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Server Status
+app.route('/').get((req,res) => {
+    res.json("Server started successfully on " + startTime);
+})
+
+// Route Authentication
+// app.use(basicAuth({
+//     users: { 'admin': 'admin' }
+// }));
+
+// User router
+const studentsRouter = require('./routes/student');
+app.use('/student',studentsRouter);
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log('Server is running on port:', port);
+})
