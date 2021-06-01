da800d826f3212942414ced29ad76b1521373fe0
Aishik Deb <debaishik14@gmail.com>
2021-06-01 21:12:19 +0530

    add login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,13 +15,15 @@ import { StudentsComponent } from './views/students/students.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { StudentDetailComponent } from './views/student-detail/student-detail.component';
 import { AddStudentComponent } from './views/add-student/add-student.component';
+import { LoginComponent } from './views/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentsComponent,
     StudentDetailComponent,
-    AddStudentComponent
+    AddStudentComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
